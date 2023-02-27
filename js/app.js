c3c2f88da8383d8afd41467c05d6b1b724da609d
Jon Cramer <joncramer96@gmail.com>
2023-02-27 06:21:31 -0500

    created search functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 baguetteBox.run('.gallery');
+
+const photoSearch = document.getElementById('search');
+
+photoSearch.addEventListener('keyup', e =>{
+    let currentValue = e.target.value.toLowerCase();
+    let photos = document.querySelectorAll('img');
+
+    photos.forEach(photo => {
+        const caption = photo.getAttribute('data-caption')
+        if (caption.toLowerCase().includes(currentValue)) {
+            photo.style.display = 'grid';
+        } else {
+            photo.style.display = 'none';
+        }
+    });
+});
+
+
+const photos = document.querySelectorAll('img');
+
+photos.forEach(photo => {
+    let randomAniDelay = Math.floor(Math.random() * 500);
+    photo.style.animation =  `fadeIn 1s .${randomAniDelay}s ease forwards`;
+})
